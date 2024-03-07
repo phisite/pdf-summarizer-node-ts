9808bf2bce7193aa9ba53a7214d50d1a635c2661
Phisit Siprasatthong <phisite@gmail.com>
2024-03-07 18:51:40 +0700

    feat: add model selector and prompt input

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
 import ollama from "ollama";
+import prompts from "prompts";
+import argv from "yargs";
+
+prompts.override(argv);
+
+const models = await ollama
+  .list()
+  .then((response) => response.models)
+  .then((response) =>
+    response.map((model) => ({ title: model.name, value: model.name }))
+  );
+
+const modelSelected = await prompts({
+  type: "select",
+  name: "model",
+  message: "Which model would you like to use?",
+  choices: models,
+  initial: 0,
+});
+
+console.log(modelSelected.model);
+
+const examplePrompt = await prompts({
+  type: "text",
+  name: "value",
+  message: "Hello, I'm a friendly assistant! Ask me anything.",
+  initial: "What is the capital of France?",
+});
 
 const response = await ollama.chat({
   model: "dolphin-phi",
@@ -9,7 +37,7 @@ const response = await ollama.chat({
     },
     {
       role: "user",
-      content: "What is the capital of France?",
+      content: examplePrompt.value,
     },
   ],
 });
